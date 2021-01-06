95c592c0241e94a708b83f893c740c00253bb56b
VanHien1234 <ghienduahau@gmail.com>
2021-01-06 17:42:18 +0700

    deit3

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,7 @@
 const Artist = require("../models/Artist");
 const Album = require("../models/Album");
 const Track = require("../models/Track");
-const Genre = require("../models/Genre");
+
 
 //ARTIST
 
